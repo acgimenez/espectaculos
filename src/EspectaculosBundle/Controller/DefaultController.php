63c89ed059570818c3b0860830c80ff13ae8a53f
Casildo Alejandro Gimenez <Alejandro.Gimenez@newsan.com.ar>
2017-07-03 20:00:35 -0300

    agrego filtros

--- src/EspectaculosBundle/Controller/DefaultController.php
+++ src/EspectaculosBundle/Controller/DefaultController.php
@@ -22,16 +22,25 @@ class DefaultController extends Controller
 
     	$form = $this->createFormBuilder()
     		->setMethod('GET')
-    		->add('nombre', 'text')
-      	->add('sala', 'text')
-      	->add('tipo', 'text') 
-  //       'entity', array(
-  //              'property'=>'nombre',
-  //              'class' => 'EspectaculosBundle:TipoEspectaculos',
-  //              'placeholder' => 'seleccionar',
-  //              'empty_value' => 0,
-   //         ))
+    		->add('nombre', 'text', array('required' => false))
+
+       	->add('sala',  'entity', array(
+                 'property'=>'nombre',
+                'class' => 'EspectaculosBundle:Sala',
+                'placeholder' => 'seleccionar',
+                'empty_value' => 0,
+                'required' => false
+            ))
+ 
+      	->add('tipo', 'entity', array(
+                 'property'=>'nombre',
+                'class' => 'EspectaculosBundle:TipoEspectaculos',
+                'placeholder' => 'seleccionar',
+                'empty_value' => 0,
+                'required' => false
+            ))
       	->add('fecha', 'date')
+
     		->add('Filtrar', 'submit')
   		
     		->getForm();
@@ -44,15 +53,34 @@ class DefaultController extends Controller
     	$form->handleRequest($request);
     	if ($form->isValid()) {
     		$criteria = $form->getData();
+        $qb->where('1=1');
+    		
+        if ($criteria['nombre']){
+          $qb->andwhere( $qb->expr()->like('p.nombre', '?1'))
+          ->setParameter(1, $criteria['nombre']);
+        }
+        if ($criteria['sala']){
+          $qb->andwhere( 'p.sala = ?2')
+          ->setParameter(2, $criteria['sala']);
+        }
 
-    		$qb->where( $qb->expr()->like('p.nombre', '?1'))
-               ->setParameter(1, $criteria['nombre']);
+        if ($criteria['tipo']){
+          $qb->andwhere( 'p.tipo = ?3')
+          ->setParameter(3, $criteria['tipo']);
+        }
 
-/**    	    	 and ( $qb->expr()->like('p.sala', '?2'))
-    	    	 and ( $qb->expr()->like('p.tipo', '?3'))
-    	    	 and ( $qb->expr()-> gt('p.fecha', '?4'))) //> \"2017-01-01\"
-                   ->setParameter(1, '%' . $criteria['nombre'] . '%');
-*/
+        if ($criteria['fecha']){
+          $qb->andwhere( 'p.fecha = ?4')
+          ->setParameter(4, $criteria['fecha']);
+        }
+
+
+        //$qb->andwhere( $qb->expr()->like('p.tipo', '?3'))
+        
+        //$qb->andwhere( $qb->expr()-> gt('p.fecha', '?4'))) //> \"2017-01-01\" 
+                          
+
+
     	}	
 
     	$espectaculo = $qb ->getQuery()->getResult();
@@ -60,4 +88,22 @@ class DefaultController extends Controller
         return $this->render('EspectaculosBundle:Default:index.html.twig', array('espectaculos' => $espectaculo, 'form' => $form->createView()));
     }
 
+/**
+     * Finds and displays a espectaculo entity.
+     *
+     * @Route("/mostrar-{id}", name="espectaculo_show")
+     * @Method("GET")
+     */
+/**   public function showAction(Espectaculo $espectaculo)
+    {
+//        $deleteForm = $this->createDeleteForm($espectaculo);
+
+        return $this->render('espectaculo/mostrar.html.twig', array(
+            'espectaculo' => $espectaculo,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+*/
+
 }
+
